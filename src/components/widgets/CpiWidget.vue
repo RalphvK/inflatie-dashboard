14b4350be740b735a72e6ba87197964661ce4c4c
Ralph van Kruiselbergen <git@ravk.nl>
2022-08-24 14:12:14 +0200

    split cpi widget in columns

--- src/components/widgets/CpiWidget.vue
+++ src/components/widgets/CpiWidget.vue
@@ -1,21 +1,33 @@
 <template>
   <dashboard-card color="red">
-    <widget-title color="red">
-      <template #default>
-      Consumentenprijsindex (CPI)
-      </template>
-      <template #subtitle>
-        Stijging consumentenprijzen t.o.v. juli 2021
-      </template>
-    </widget-title>
-    <widget-measure
-      icon="bi-cash-coin"
-      icon-class="mt-2"
-      value="12,5%"
-      tickerSymbol="up"
-      color="red"
-    >
-    </widget-measure>
+    <div class="flex flex-col sm:flex-row">
+
+      <!-- left column -->
+      <div class="col">
+        <widget-title color="red">
+          <template #default>
+          Consumentenprijsindex (CPI)
+          </template>
+          <template #subtitle>
+            Stijging consumentenprijzen t.o.v. juli 2021
+          </template>
+        </widget-title>
+        <widget-measure
+          icon="bi-cash-coin"
+          icon-class="mt-2"
+          value="12,5%"
+          tickerSymbol="up"
+          color="red"
+        >
+        </widget-measure>
+      </div>
+
+      <!-- right column -->
+      <div class="col">
+        Visualisation will go here
+      </div>
+
+    </div>
   </dashboard-card>
 </template>
 
