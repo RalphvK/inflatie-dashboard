ddaa507f3b55188f481a3f2475e2fa31fd8ec9c8
Ralph van Kruiselbergen <git@ravk.nl>
2022-08-24 17:12:50 +0200

    add submeasure component

---
+++ src/components/SubMeasure.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="sub-measure flex items-baseline gap-2">
+    <measure-value :value="value" variant="sub" :color="color + '-900'"/>
+    <div :class="`description text-sm font-serif font-medium text-${color}-900/80`">
+     <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import MeasureValue from '@/components/MeasureValue.vue';
+
+export default {
+  components: {
+    MeasureValue
+  },
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    color: {
+      color: String,
+      default: 'slate'
+    }
+  }
+}
+</script>
