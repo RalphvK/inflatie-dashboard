829d76867e979824d9699df8b75442e2d9f4a47f
Ralph van Kruiselbergen <git@ravk.nl>
2022-08-24 11:43:42 +0200

    add option to set additional icon classes

--- src/components/MeasureIcon.vue
+++ src/components/MeasureIcon.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="['icon-circle', 'bg-'+backgroundColor, 'text-'+foregroundColor]">
-    <i :class="['bi', icon]"></i>
+    <i :class="['bi', icon, iconClass]"></i>
   </div>
 </template>
 
@@ -8,6 +8,10 @@
 export default {
   props: {
     icon: {
+      type: String,
+      required: true
+    },
+    iconClass: {
       type: String,
       default: null
     },
@@ -30,6 +34,7 @@ export default {
 <style scoped lang="scss">
 .icon-circle {
   @apply flex justify-center items-center rounded-full w-12 h-12;
+  overflow: hidden;
 
   i {
     font-size: v-bind(iconSize);
